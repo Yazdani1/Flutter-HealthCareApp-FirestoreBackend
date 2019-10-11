e83edd631e91162e94ac395fd4e6dc99b0d5eed3
Yazdani <yaz4dani@gmail.com>
2019-10-11 21:19:16 +0600

    Material Design

--- lib/Home.dart
+++ lib/Home.dart
@@ -10,7 +10,6 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-
       appBar: new AppBar(
         title: new Text("Health Care"),
         backgroundColor: Color(0xFF222240)
