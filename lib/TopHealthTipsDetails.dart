72b130c352f6b9b97d5924c03937b7a02a6a899e
Yazdani <yaz4dani@gmail.com>
2019-10-17 12:48:20 +0600

    HealthTips

---
+++ lib/TopHealthTipsDetails.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:async';
+
+
+class TopHealthTipsDetails extends StatefulWidget {
+  @override
+  _TopHealthTipsDetailsState createState() => new _TopHealthTipsDetailsState();
+}
+
+class _TopHealthTipsDetailsState extends State<TopHealthTipsDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      
+    );
+  }
+}
+
+
+
+
+
+
