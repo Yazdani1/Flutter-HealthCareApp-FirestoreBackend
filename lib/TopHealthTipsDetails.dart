b7a5858a6283f8e5fcfbd742973c1562292bbb9a
Yazdani <yaz4dani@gmail.com>
2019-10-17 12:56:43 +0600

    HealthTips

--- lib/TopHealthTipsDetails.dart
+++ lib/TopHealthTipsDetails.dart
@@ -22,6 +22,29 @@ class _TopHealthTipsDetailsState extends State<TopHealthTipsDetails> {
         title: new Text("Top HealthTips Details"),
         backgroundColor: Color(0xff222240),
       ),
+      backgroundColor: Color(0xFF222240),
+      body: new ListView(
+        children: <Widget>[
+
+          //first container start
+
+          new Container(
+            margin: EdgeInsets.all(10.0),
+            child: new ClipRRect(
+              borderRadius: BorderRadius.circular(15.0),
+              child: new Image.network(widget.snapshot.data["image"],
+              height: 250.0,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+
+          //first container end
+
+
+        ],
+      ),
+
       
     );
   }
