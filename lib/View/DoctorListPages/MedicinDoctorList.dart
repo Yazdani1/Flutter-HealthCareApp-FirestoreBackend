831b7c62d52c4a5b5d269b113d755eff92e95e5e
Yazdani <yaz4dani@gmail.com>
2019-10-24 18:32:20 +0600

    HealthTips

--- lib/View/DoctorListPages/MedicinDoctorList.dart
+++ lib/View/DoctorListPages/MedicinDoctorList.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 
 class MedicinDoctorList extends StatefulWidget {
@@ -7,11 +9,21 @@ class MedicinDoctorList extends StatefulWidget {
 }
 
 class _MedicinDoctorListState extends State<MedicinDoctorList> {
+
+  Future getAllpost() async {
+    var fr = Firestore.instance;
+    QuerySnapshot snap = await fr.collection("MedicinDoctorList")
+        .getDocuments();
+    return snap.documents;
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+
       appBar: new AppBar(
-          title: new Text("MedicinDoctorList"),
+        title: new Text("MedicinDoctorList"),
         flexibleSpace: Container(
           decoration: BoxDecoration(
             gradient: LinearGradient(
@@ -19,7 +31,7 @@ class _MedicinDoctorListState extends State<MedicinDoctorList> {
               end: Alignment.centerRight,
               colors: <Color>[
                 Colors.red,
-                Colors.amber
+                Colors.amber,
               ],
             ),
           ),
@@ -32,7 +44,6 @@ class _MedicinDoctorListState extends State<MedicinDoctorList> {
           gradient: LinearGradient(
             begin: Alignment.topRight,
             end: Alignment.bottomLeft,
-            stops: [0.1, 0.5, 0.7, 0.9],
             colors: [
               Colors.deepOrange[400],
               Colors.lightGreen[200],
@@ -41,8 +52,31 @@ class _MedicinDoctorListState extends State<MedicinDoctorList> {
             ],
           ),
         ),
-
-
+        child: new FutureBuilder(
+            future: getAllpost(),
+            builder: (_, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: new Text("Data Loading..."),
+                );
+              } else {
+                return ListView.builder(
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        elevation: 10.0,
+                        margin: EdgeInsets.all(5.0),
+                        child: Row(
+                          children: <Widget>[
+                            
+                          ],
+                        ),
+                      );
+                    }
+                );
+              }
+            }
+        ),
       ),
 
     );
