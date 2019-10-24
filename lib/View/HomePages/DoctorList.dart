87747b1da27ba935b0a3c24eaa63e33fbe46c6a1
Yazdani <yaz4dani@gmail.com>
2019-10-24 17:36:35 +0600

    HealthTips

--- lib/View/HomePages/DoctorList.dart
+++ lib/View/HomePages/DoctorList.dart
@@ -54,10 +54,10 @@ class _DoctorListState extends State<DoctorList> {
                   NetworkImage(
                       'https://images.pexels.com/photos/965989/pexels-photo-965989.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'),
                   NetworkImage(
-                    'https://images.pexels.com/photos/264591/pexels-photo-264591.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+                      'https://images.pexels.com/photos/264591/pexels-photo-264591.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
                   ),
                   NetworkImage(
-                    'https://images.pexels.com/photos/264829/pexels-photo-264829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+                      'https://images.pexels.com/photos/264829/pexels-photo-264829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
                   ),
 
                 ],
@@ -65,13 +65,188 @@ class _DoctorListState extends State<DoctorList> {
             ),
           ),
           //first container end..
-          
+
+          //second container start
+          doctortypes(),
+          //second container end
+
+
         ],
       ),
 
 
-
     );
   }
+
+  Widget doctortypes() {
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      height: MediaQuery
+          .of(context)
+          .size
+          .height/2,
+      child: new Column(
+        children: <Widget>[
+
+          //first container start
+          new Container(
+            height: 150.0,
+            child: new Row(
+              children: <Widget>[
+
+                new Expanded(
+                  flex: 1,
+                  child: new Container(
+                    decoration: new BoxDecoration(
+                      color: Color(0xFF272B4A),
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    child: new Column(
+                      children: <Widget>[
+
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(15.0),
+                          child: new Image.network(
+                            "https://images.pexels.com/photos/159211/headache-pain-pills-medication-159211.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+                            height: 110.0,
+                            width: MediaQuery.of(context).size.width,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        new SizedBox(height: 10.0,),
+
+                        Text("Psychiatrist Doctor",
+                          style: TextStyle(
+                              fontSize: 16.0,
+                              color: Colors.lightGreen
+                          ),
+                        )
+
+                      ],
+                    ),
+                  ),
+                ),
+                new SizedBox(width: 10.0,),
+
+                new Expanded(
+                  flex: 1,
+                  child: new Container(
+                    decoration: new BoxDecoration(
+                      color: Color(0xFF272B4A),
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    child: new Column(
+                      children: <Widget>[
+
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(15.0),
+                          child: new Image.network(
+                            "https://images.pexels.com/photos/356040/pexels-photo-356040.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+                            height: 110.0,
+                            width: MediaQuery.of(context).size.width,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        new SizedBox(height: 10.0,),
+
+                        Text("Endocrinologist Doctor",
+                          style: TextStyle(
+                              fontSize: 16.0,
+                              color: Colors.lightGreen
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+
+
+              ],
+            ),
+          ),
+          //first container end
+
+          new SizedBox(height: 10.0,),
+
+          new Container(
+            height: 150.0,
+            child: new Row(
+              children: <Widget>[
+
+                new Expanded(
+                  flex: 1,
+                  child: new Container(
+                    decoration: new BoxDecoration(
+                      color: Color(0xFF272B4A),
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    child: new Column(
+                      children: <Widget>[
+
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(15.0),
+                          child: new Image.network(
+                            "https://images.pexels.com/photos/42273/doctor-medical-medicine-health-42273.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+                            height: 110.0,
+                            width: MediaQuery.of(context).size.width,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        new SizedBox(height: 10.0,),
+
+                        Text("Neurologist Doctor",
+                          style: TextStyle(
+                              fontSize: 16.0,
+                              color: Colors.lightGreen
+                          ),
+                        )
+
+                      ],
+                    ),
+                  ),
+                ),
+                new SizedBox(width: 10.0,),
+
+                new Expanded(
+                  flex: 1,
+                  child: new Container(
+                    decoration: new BoxDecoration(
+                      color: Color(0xFF272B4A),
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    child: new Column(
+                      children: <Widget>[
+
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(15.0),
+                          child: new Image.network(
+                            "https://images.pexels.com/photos/4154/clinic-doctor-health-hospital.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+                            height: 110.0,
+                            width: MediaQuery.of(context).size.width,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        new SizedBox(height: 10.0,),
+
+                        Text("Medicin Doctor",
+                          style: TextStyle(
+                              fontSize: 16.0,
+                              color: Colors.lightGreen
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+        ],
+      ),
+    );
+  }
+
+
 }
 
