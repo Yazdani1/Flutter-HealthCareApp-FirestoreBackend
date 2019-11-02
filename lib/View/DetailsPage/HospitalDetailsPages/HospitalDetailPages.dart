c7e4407b7e820cd57c2de64423a9d6c7b350b397
Yazdani <yaz4dani@gmail.com>
2019-11-02 20:02:07 +0600

    HospitalList

--- lib/View/DetailsPage/HospitalDetailsPages/HospitalDetailPages.dart
+++ lib/View/DetailsPage/HospitalDetailsPages/HospitalDetailPages.dart
@@ -451,6 +451,11 @@ class _HospitalDetailPagesState extends State<HospitalDetailPages> {
                     ],
                   ),
                 ),
+
+
+
+
+                
                 SizedBox(height: 6.0,),
                 //end first container
                 //second container start
