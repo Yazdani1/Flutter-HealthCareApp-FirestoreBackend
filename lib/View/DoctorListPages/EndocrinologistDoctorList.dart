7c2df0834fb63be8c7fe8c0439aab9b20ba954b7
Yazdani <yaz4dani@gmail.com>
2019-11-11 21:45:50 +0600

    HospitalList

--- lib/View/DoctorListPages/EndocrinologistDoctorList.dart
+++ lib/View/DoctorListPages/EndocrinologistDoctorList.dart
@@ -174,9 +174,10 @@ class _EndocrinologistDoctorListState extends State<EndocrinologistDoctorList> {
                                       )
                                   ),
                                   SizedBox(width: 10.0,),
-                                  
+
                                   Expanded(
                                     flex: 3,
+                                    
                                     child: Column(
                                       crossAxisAlignment: CrossAxisAlignment
                                           .start,
